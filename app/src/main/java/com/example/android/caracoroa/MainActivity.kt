b58f0096a0731c963730ba6018f56ba62639ae43
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2018-05-28 00:27:43 -0300

    Criação da navegação entre as telas e adição da lógica do aplicativo

--- app/src/main/java/com/example/android/caracoroa/MainActivity.kt
+++ app/src/main/java/com/example/android/caracoroa/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.android.caracoroa
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image_jogar.setOnClickListener{
+
+            val intent = Intent(this, ResultadoActivity:: class.java)
+
+            //gerar número randômico
+            val numero = Random().nextInt(2)
+
+            //enviar número para a próxima activity
+            intent.putExtra("numero", numero)
+
+            startActivity(intent)
+        }
     }
 }
